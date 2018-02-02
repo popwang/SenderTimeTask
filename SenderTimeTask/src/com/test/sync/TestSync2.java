41d1fa8559264073add36d28f330d2fd1731cba2
wangyubo <274388924@qq.com>
2018-02-02 17:05:45 +0800

    1

--- SenderTimeTask/src/com/test/sync/TestSync2.java
+++ SenderTimeTask/src/com/test/sync/TestSync2.java
@@ -1,24 +1,37 @@
 package com.test.sync;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TestSync2 implements Runnable {
 	int  b = 100;
 	synchronized void m1() throws InterruptedException{
 		b = 1000;
-		Thread.sleep(500);//6
+//		Thread.sleep(5000);//6
 		System.out.println("b="+b);
 	}
 	
 	synchronized void m2() throws InterruptedException{
-		Thread.sleep(250);//5
 		b = 2000;
+//		System.out.println("m2="+b);
 	}
 	
 	public static void main(String[] args) throws InterruptedException{
-		TestSync2 tt = new TestSync2();
-		Thread t = new Thread(tt);//1
-		t.start();//2
-		tt.m2();//3
-		System.out.println("main thread b="+tt.b);//4
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				TestSync2 tt = new TestSync2();
+				Thread t = new Thread(tt);//1
+				t.start();//2
+				try {
+					tt.m2();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}//3
+				System.out.println("main thread b="+tt.b);//4
+			}
+		}, 1000,1000);
 	}
 	
 	@Override
